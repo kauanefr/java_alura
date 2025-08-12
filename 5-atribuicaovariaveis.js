e4dc1a863d83075a74c5f9fa30dc6b3e24b7af06
kauane <kauane.saturnino.franca@escola.pr.gov.br>
2025-08-12 14:30:08 -0300

    Add files via upload

---
+++ 5-atribuicaovariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const Primeironome = "Ricardo";
+const sobrenome = "Bugan";
+
+//console.log(primeironome + sobrenome);
+console.log(nome + sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`); 
+
+let contador=0 
