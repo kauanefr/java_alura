0edd5aa07c051f9f65a56d737a55c1766f90c83f
kauane <kauane.saturnino.franca@escola.pr.gov.br>
2025-08-14 16:25:47 -0300

    Add files via upload

---
+++ 6-listas.js
@@ -0,0 +1,22 @@
+console.log('trabalhando com listas');
+//const salvador = 'Salvador';
+//const saopaulo = 'Sao paulo';
+//const riodejaneiro = 'Rio de Janeiro';
+const listasdeDestinos = new Array;(
+`Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+
+);    
+listaDeDestinos.push(`Curitiba`) //adicionado um item na lista
+console.log(`Destinos possíveis`);
+console.log(salvador, saoPaulo, rioDeJaneiro)
+console.log(listasdeDestinos);
+
+listaDeDestinos=2;
+
+listasdeDestinos.splice(1,1);
+console.log(listasdeDestinos);
+
+
+console.log(listasdeDestinos[1], listasdeDestinos[0]);
