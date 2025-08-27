6d45b10dc789b1ab2a3a359f2322fe8b2986d355
kauane <kauane.saturnino.franca@escola.pr.gov.br>
2025-08-27 09:46:55 -0300

    Add files via upload

--- 8-loops.js
+++ 8-loops.js
@@ -1,28 +1,41 @@
-console.log('\ntrabalhando com condicionais');
-const listasdeDestinos = new Array(
-`Salvador`,
+console.log(`\nTrabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
     `São Paulo`,
     `Rio de Janeiro`
-);    
+);
 
-const idadeComprador =18;
-const estaAcompanhada =false;
-let temPassagemComprada =false;
-const destino = "São Paulo";
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Curitiba";
 
+console.log("\n Destinos possíveis:");
+console.log(listaDeDestinos);
 
-console.log("\n Destinos possíveis");
-console.log(listasdeDestinos);
+const podeComprar  = idadeComprador >= 18 || estaAcompanhada == true;
 
-
-const podeComprar =  idadeComporador >= 18 || estaAcompanhada == true;
-
-let contador = 0
+let contador = 0;
+let destinoExiste = false;
 while(contador<3){
     if(listaDeDestinos[contador] == destino){
-        console.log("Destino existe") 
-    }else{ 
-        console.log("Destino não existe");
+        destinoExiste = true;
+        break;
     }
     contador += 1;
-} 
+}
+
+console.log("Destino exite: ", destinoExiste);
+
+if(podeComprar && destinoExiste){
+    console.log("Boa Viagem");
+}else{
+    console.log("Desculpe tivemos um erro!");
+}
+
+for(let i  = 0 ; i < 3 ; i++){
+    if(listaDeDestinos[i] == destino){
+        destinoExiste = true;
+    }
+   
+}
